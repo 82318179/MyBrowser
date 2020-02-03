49c89efc99a0913211ccc5fd37f1de0bf4cb663f
king <82318179@qq.com>
2020-02-03 22:18:53 +0800

    new-window

--- src/components/Chrome/ChromeFrame.vue
+++ src/components/Chrome/ChromeFrame.vue
@@ -49,16 +49,20 @@ export default {
     if (this.page.location) {
       this.navigateTo(this.page.location);
     }
+    let webContents;
     this.$refs.webview.addEventListener("dom-ready", () => {
-      const webContents = this.$refs.webview.getWebContents();
-      webContents.on("new-window", (e, url) => {
-        if (!new URL(url).host) {
-          // Handle newUrl = 'about:blank' in some cases
-          return;
-        }
-        e.preventDefault();
-        this.bPage.onNewTab(url);
-      });
+      if (!webContents) {
+        webContents = this.$refs.webview.getWebContents();
+        webContents.on("new-window", (e, url) => {
+          if (!new URL(url).host) {
+            // Handle newUrl = 'about:blank' in some cases
+            return;
+          }
+          e.preventDefault();
+          console.log(11); //会执行两次
+          this.bPage.onNewTab(url);
+        });
+      }
     });
   }
 };
